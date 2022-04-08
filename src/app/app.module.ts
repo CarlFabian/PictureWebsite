b76d948fda9dc7ef151081455fb455b72dd447cb
gustafwetterlund <58036889+gustafwetterlund@users.noreply.github.com>
2022-04-08 14:43:01 +0200

    XD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     MatSliderModule,
