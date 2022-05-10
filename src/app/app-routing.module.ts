d11dad7ba53a10ab61e987a2f7ddbcd4d8a76bee
CarlFabian <60578873+CarlFabian@users.noreply.github.com>
2022-05-10 14:30:48 +0200

    added upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,15 @@ import {RegisterUserPageComponent} from "./register-user-page/register-user-page
 import {AccountPageComponent} from "./account-page/account-page.component";
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {SearchAreaComponent} from "./search-area/search-area.component";
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
+  { path: '',   redirectTo: '/search', pathMatch: 'full' },
   {path: 'register-user-page', component: RegisterUserPageComponent},
   {path:'account-page', component: AccountPageComponent},
   {path:'login-page', component:LoginPageComponent},
-  {path:'search-area', component:SearchAreaComponent}
+  {path:'search', component:SearchAreaComponent},
+  {path:'upload', component:ImageComponent}
 ];
 
 @NgModule({
