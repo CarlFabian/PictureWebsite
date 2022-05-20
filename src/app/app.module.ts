931d3f48539a25320cec32d6836de6d3af551cc6
CarlFabian <oscfre-9@student.ltu.se>
2022-05-20 12:21:45 +0200

    removed unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HomepageComponent } from './homepage/homepage.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
 import { AccountPageComponent } from './account-page/account-page.component';
@@ -35,7 +34,6 @@ import {ToolbarComponent} from "./toolbar/toolbar.component";
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
     LoginPageComponent,
     RegisterUserPageComponent,
     AccountPageComponent,
